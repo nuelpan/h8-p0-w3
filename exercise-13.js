34ac9c61e60586332fb06cb19fd610251d86d2e8
Immanuel Pandiangan <nuelpan@gmail.com>
2019-10-02 15:04:49 +0700

    add exercise-13.js

---
+++ exercise-13.js
@@ -0,0 +1,36 @@
+function targetTerdekat(arr) {
+  // you can only write your code here!
+  var indexO = 0;
+  var indexX = [];
+  for (var i = 0; i < arr.length; i++) {
+    // cari index o nya dan kumpulkan index x nya didalam indexX array
+    if (arr[i] === "o") {
+      indexO = i;
+    } else if (arr[i] === "x") {
+      indexX.push(i);
+    }
+  }
+
+  // hitung selisih indexO dengan setiap item di dalam array indexX
+  // dan sort secara ascending, otomatis selisih yang paling kecil akan
+  // muncul di index[0], kalau index[0] undefined berarti x tidak ditemukan
+  var hasil = indexX
+    .map(function(item) {
+      return Math.abs(item - indexO);
+    })
+    .sort(function(a, b) {
+      return a - b;
+    });
+  if (hasil[0] === undefined) {
+    return 0;
+  } else {
+    return hasil[0];
+  }
+}
+
+// TEST CASES
+console.log(targetTerdekat([" ", " ", "o", " ", " ", "x", " ", "x"])); // 3
+console.log(targetTerdekat(["o", " ", " ", " ", "x", "x", "x"])); // 4
+console.log(targetTerdekat(["x", " ", " ", " ", "x", "x", "o", " "])); // 1
+console.log(targetTerdekat([" ", " ", "o", " "])); // 0
+console.log(targetTerdekat([" ", "o", " ", "x", "x", " ", " ", "x"])); // 2
