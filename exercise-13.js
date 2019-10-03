886df839c88fc0ecbbeffa996701204b2f193d9b
Immanuel Pandiangan <nuelpan@gmail.com>
2019-10-03 13:21:06 +0700

    change exercise-13 to not using built in function

--- exercise-13.js
+++ exercise-13.js
@@ -2,6 +2,8 @@ function targetTerdekat(arr) {
   // you can only write your code here!
   var indexO = 0;
   var indexX = [];
+  var hasil = [];
+
   for (var i = 0; i < arr.length; i++) {
     // cari index o nya dan kumpulkan index x nya didalam indexX array
     if (arr[i] === "o") {
@@ -11,21 +13,34 @@ function targetTerdekat(arr) {
     }
   }
 
-  // hitung selisih indexO dengan setiap item di dalam array indexX
-  // dan sort secara ascending, otomatis selisih yang paling kecil akan
-  // muncul di index[0], kalau index[0] undefined berarti x tidak ditemukan
-  var hasil = indexX
-    .map(function(item) {
-      return Math.abs(item - indexO);
-    })
-    .sort(function(a, b) {
-      return a - b;
-    });
+  for (i = 0; i < indexX.length; i++) {
+    hasil.push(Math.abs(indexX[i] - indexO));
+  }
+  hasil.sort(function(a, b) {
+    return a - b;
+  });
+
   if (hasil[0] === undefined) {
     return 0;
   } else {
     return hasil[0];
   }
+
+  // hitung selisih indexO dengan setiap item di dalam array indexX
+  // dan sort secara ascending, otomatis selisih yang paling kecil akan
+  // muncul di index[0], kalau index[0] undefined berarti x tidak ditemukan
+  // var hasil = indexX
+  //   .map(function(item) {
+  //     return Math.abs(item - indexO);
+  //   })
+  //   .sort(function(a, b) {
+  //     return a - b;
+  //   });
+  // if (hasil[0] === undefined) {
+  //   return 0;
+  // } else {
+  //   return hasil[0];
+  // }
 }
 
 // TEST CASES
